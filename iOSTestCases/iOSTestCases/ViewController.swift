e82816bf3cab5cc0d6b09ca6482bf14bbc5657a5
buddhdev-jay <jay.b@simformsolutions.com>
2022-08-05 13:53:36 +0530

    Base Project Setup

---
+++ iOSTestCases/iOSTestCases/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTestCases
+//
+//  Created by Jay Buddhdev on 05/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
