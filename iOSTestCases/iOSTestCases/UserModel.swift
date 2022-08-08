17b3cba0014bf07948b6a8dad365d8dce9fcaf55
buddhdev-jay <jay.b@simformsolutions.com>
2022-08-08 10:42:00 +0530

    Registration Test Case Done

---
+++ iOSTestCases/iOSTestCases/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  iOSTestCases
+//
+//  Created by Jay Buddhdev on 05/08/22.
+//
+
+import Foundation
+
+struct User {
+    var name : String
+    var email : String
+    var password : String
+}
---
+++ iOSTestCases/iOSTestCases/UserViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  UserViewModel.swift
+//  iOSTestCases
+//
+//  Created by Jay Buddhdev on 05/08/22.
+//
+
+import Foundation
+
+class UserViewModel {
+    
+    func validation(userModel:User) -> Bool {
+        if (userModel.name.isEmpty) {
+            return false
+        } else if (userModel.email.isEmpty) {
+            return false
+        } else if (!isValidEmail(testStr: userModel.email)){
+            return false
+        } else if (userModel.password.isEmpty){
+            return false
+        }
+        return true
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+
+        print("validate emilId: \(testStr)")
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let result = emailTest.evaluate(with: testStr)
+        return result
+
+    }
+}
